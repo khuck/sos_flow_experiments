8b792aa4ba1da7d77333bc633c1f5ed7340b361f
Kevin Huck <khuck@cs.uoregon.edu>
2017-05-14 21:39:50 -0700

    cleaning up use of slurm

--- call_and_response/go.sh
+++ call_and_response/go.sh
@@ -4,6 +4,8 @@
 #SBATCH --cores-per-socket=14 
 #SBATCH --partition=defq 
 
+export I_MPI_PMI_LIBRARY=/cm/shared/apps/slurm/16.05.8/lib64/libpmi.so
+
 cd /home/khuck/src/sos_flow_experiments/call_and_response
 module load gcc intel slurm
 export LD_LIBRARY_PATH=/cm/local/apps/gcc/6.1.0/lib64:$LD_LIBRARY_PATH
@@ -14,6 +16,7 @@ cwd=`pwd`
 num_listeners=2
 app_ranks_per_node=1
 app_ranks=2
+cpus_per_task=28
 
 export sos_cmd="${sosbin}/sosd -l ${num_listeners} -a 1 -w ${cwd}"
 if [ ${num_listeners} == 0 ] ; then
@@ -33,16 +36,17 @@ rm -rf sosd.* profile* start0000*
 
 # launch the aggregator
 # ${sos_cmd} -k 0 -r aggregator &
-mpirun -np 1 --host n002 ${sos_cmd} -k 0 -r aggregator &
+#mpirun -np 1 --host n002 ${sos_cmd} -k 0 -r aggregator &
+srun -n 1 -c ${cpus_per_task} --nodelist=n002 ${sos_cmd} -k 0 -r aggregator &
 #${sos_cmd} -k 0 -r aggregator &
 
 #sleep 8
 
-ldd main
-mpirun -np ${app_ranks} -ppn ${app_ranks_per_node} -hosts n003,n004 ./main
+#mpirun -np ${app_ranks} -ppn ${app_ranks_per_node} -hosts n003,n004 ./main
+srun -n ${app_ranks} -c ${cpus_per_task} --nodelist=n003,n004 ./main
 #gdb --args ./main
 #./main
 
-mpirun -np 3 -ppn 1 killall -9 sosd
+#mpirun -np 3 -ppn 1 killall -9 sosd
 #mpirun -np 3 -ppn 1 ${sosbin}/sosd_stop
 ${sosbin}/showdb
