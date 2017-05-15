7ca72c980ae53c962e5d5ffa4415fce358b9f472
Kevin Huck <khuck@cs.uoregon.edu>
2017-05-15 09:39:05 -0700

    All working on ktau

--- call_and_response/openmpi.sh
+++ call_and_response/openmpi.sh
@@ -1,13 +1,13 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 hostname=`hostname`
 sosbin=/home/khuck/src/sos_flow/build-linux/bin
 cwd=`pwd`
 num_listeners=0
-app_ranks_per_node=1
-app_ranks=2
+app_ranks_per_node=4
+app_ranks=4
 
-export sos_cmd="${sosbin}/sosd -l ${num_listeners} -a 1 -w ${cwd}"
+export sos_cmd="taskset -c 7 ${sosbin}/sosd -l ${num_listeners} -a 1 -w ${cwd}"
 if [ ${num_listeners} == 0 ] ; then
   export SOS_FORK_COMMAND="${sos_cmd} -k @LISTENER_RANK@ -r aggregator"
   export SOS_LISTENER_RANK_OFFSET=0
@@ -26,17 +26,9 @@ rm -rf sosd.* profile* start0000*
 # launch the aggregator
 # ${sos_cmd} -k 0 -r aggregator &
 #mpirun -np 1 --host n002 ${sos_cmd} -k 0 -r aggregator &
-#srun -n 1 -N 1 -c 28 --nodelist=${SLURMD_NODENAME} ${sos_cmd} -k 0 -r aggregator &
-#${sos_cmd} -k 0 -r aggregator &
 
 sleep 2
 
-mpirun -np ${app_ranks} -ppn ${app_ranks_per_node} ./main
-#srun -n ${app_ranks} -c ${cpus_per_task} -nodelist=n003,n004 ./main
-#srun -n ${app_ranks} -N ${app_ranks_per_node} --exclude=${SLURMD_NODENAME} ./main
-#gdb --args ./main
-#./main
+mpirun -np ${app_ranks} --map-by core ./main
 
-#mpirun -np 3 -ppn 1 killall -9 sosd
-#mpirun -np 3 -ppn 1 ${sosbin}/sosd_stop
 ${sosbin}/showdb
