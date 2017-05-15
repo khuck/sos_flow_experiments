80c3cf47d9d65c4688e1c4a418f2cb6e7cf22738
Kevin Huck <khuck@cs.uoregon.edu>
2017-05-14 22:13:52 -0700

    I give up

--- call_and_response/go.sh
+++ call_and_response/go.sh
@@ -16,7 +16,6 @@ cwd=`pwd`
 num_listeners=2
 app_ranks_per_node=1
 app_ranks=2
-cpus_per_task=28
 
 export sos_cmd="${sosbin}/sosd -l ${num_listeners} -a 1 -w ${cwd}"
 if [ ${num_listeners} == 0 ] ; then
@@ -37,14 +36,14 @@ rm -rf sosd.* profile* start0000*
 # launch the aggregator
 # ${sos_cmd} -k 0 -r aggregator &
 #mpirun -np 1 --host n002 ${sos_cmd} -k 0 -r aggregator &
-srun -n 1 -c 28 ${sos_cmd} -k 0 -r aggregator &
+srun -n 1 -N 1 -c 28 --nodelist=${SLURMD_NODENAME} ${sos_cmd} -k 0 -r aggregator &
 #${sos_cmd} -k 0 -r aggregator &
 
-#sleep 8
+sleep 2
 
 #mpirun -np ${app_ranks} -ppn ${app_ranks_per_node} -hosts n003,n004 ./main
 #srun -n ${app_ranks} -c ${cpus_per_task} -nodelist=n003,n004 ./main
-srun -n ${app_ranks} -c ${cpus_per_task} ./main
+srun -n ${app_ranks} -N ${app_ranks_per_node} --exclude=${SLURMD_NODENAME} ./main
 #gdb --args ./main
 #./main
 
