31a3e3a9678aa1b8f19f4f78523250ee6cd6cdc0
Kevin Huck <khuck@cs.uoregon.edu>
2018-04-16 18:26:40 -0700

    Updating summary to include HWM and RSS

--- sos_scripts/TAU_profile_summary/sos_flow_analysis.py
+++ sos_scripts/TAU_profile_summary/sos_flow_analysis.py
@@ -146,9 +146,15 @@ def get_group_metric(c,timer_type,metric,group,prog_name):
     return np.sum(alltime)
 
 # How many bytes was sent in <group>?
-def get_group_counter(c,group,prog_name,high_res):
+def get_group_counter(c,group,prog_name,high_res,stat):
     if high_res == 'off':
         sql_statement = "select cast(value as decimal), value_name, comm_rank, max(frame) from viewCombined where value_name like '%Total:" + group + "' and prog_name = '" + prog_name + "' group by value_name, comm_rank;"
+        try_execute(c,sql_statement);
+        all_rows = c.fetchall()
+        bytes = np.array([x[0] for x in all_rows]).astype(np.float)
+        sum = np.sum(bytes)
+    elif stat != 'Total':
+        sql_statement = "select cast(value as decimal), value_name, comm_rank, max(frame) from viewCombined where value_name like '%" + stat + ":" + group + "' and prog_name = '" + prog_name + "' group by value_name, comm_rank;"
         try_execute(c,sql_statement);
         all_rows = c.fetchall()
         bytes = np.array([x[0] for x in all_rows]).astype(np.float)
@@ -194,34 +200,38 @@ def sos_flow_analysis(run_dir):
             user_exclusive = (get_group_metric(c,"exclusive","TIME","TAU_USER",n_str)/1000000)/r
             mpi_exclusive = (get_group_metric(c,"exclusive","TIME","MPI",n_str)/1000000)/r
             adios_exclusive = (get_group_metric(c,"exclusive","TIME","TAU_IO",n_str)/1000000)/r
-            mpi_collective_bytes = get_group_counter(c,"Collective Bytes Sent",n_str,h_str)
-            mpi_recv_bytes = get_group_counter(c,"MPI Receive Bytes",n_str,h_str)
-            mpi_send_bytes = get_group_counter(c,"MPI Send Bytes",n_str,h_str)
-            io_read_bytes = get_group_counter(c,"IO Bytes Read",n_str,h_str)
-            io_write_bytes = get_group_counter(c,"IO Bytes Written",n_str,h_str)
-            adios_write_bytes = get_group_counter(c,"ADIOS data size",n_str,h_str)
+            mpi_collective_bytes = get_group_counter(c,"Collective Bytes Sent",n_str,h_str,"Total")
+            mpi_recv_bytes = get_group_counter(c,"MPI Receive Bytes",n_str,h_str,"Total")
+            mpi_send_bytes = get_group_counter(c,"MPI Send Bytes",n_str,h_str,"Total")
+            io_read_bytes = get_group_counter(c,"IO Bytes Read",n_str,h_str,"Total")
+            io_write_bytes = get_group_counter(c,"IO Bytes Written",n_str,h_str,"Total")
+            adios_write_bytes = get_group_counter(c,"ADIOS data size",n_str,h_str,"Total")
         else:
             total_inclusive = (get_group_metric(c,"inclusive","TIME","TAU_USER:.TAU application",n_str)/1000000)/r
             mpi_exclusive = (get_group_metric(c,"exclusive","TIME","MPI:MPI_",n_str)/1000000)/r
             adios_exclusive = (get_group_metric(c,"exclusive","TIME","TAU_IO:adios_",n_str)/1000000)/r
             user_exclusive = total_inclusive - (mpi_exclusive + adios_exclusive)
-            mpi_collective_bytes = get_group_counter(c,"Message size for %",n_str,h_str)
-            mpi_recv_bytes = get_group_counter(c,"Message size received from all nodes",n_str,h_str)
-            mpi_send_bytes = get_group_counter(c,"Message size sent to all nodes",n_str,h_str)
-            io_read_bytes = get_group_counter(c,"Bytes Read",n_str,h_str)
-            io_write_bytes = get_group_counter(c,"Bytes Written",n_str,h_str)
-            adios_write_bytes = get_group_counter(c,"ADIOS data size",n_str,h_str)
+            mpi_collective_bytes = get_group_counter(c,"Message size for %",n_str,h_str,"Total")
+            mpi_recv_bytes = get_group_counter(c,"Message size received from all nodes",n_str,h_str,"Total")
+            mpi_send_bytes = get_group_counter(c,"Message size sent to all nodes",n_str,h_str,"Total")
+            io_read_bytes = get_group_counter(c,"Bytes Read",n_str,h_str,"Total")
+            io_write_bytes = get_group_counter(c,"Bytes Written",n_str,h_str,"Total")
+            adios_write_bytes = get_group_counter(c,"ADIOS data size",n_str,h_str,"Total")
+            high_water_mark = get_group_counter(c,"Peak Memory Usage Resident Set Size (VmHWM) (KB)",n_str,h_str,"Max")
+            resident_set_size = get_group_counter(c,"Memory Footprint (VmRSS) (KB)",n_str,h_str,"Mean")
         if True:
             print("\t","Total time :",total_inclusive,"seconds")
             print("\t","User  time :",user_exclusive,"seconds")
             print("\t","MPI   time :",mpi_exclusive,"seconds (",(mpi_exclusive/total_inclusive)*100,"% )")
             print("\t","ADIOS time :",adios_exclusive,"seconds (",(adios_exclusive/total_inclusive)*100,"% )")
-            print("\t","MPI collective bytes   :",mpi_collective_bytes)
-            print("\t","MPI P2P bytes sent     :",mpi_send_bytes)
-            print("\t","MPI P2P bytes received :",mpi_recv_bytes)
-            print("\t","Posix IO bytes read    :",io_read_bytes)
-            print("\t","Posix IO bytes written :",io_write_bytes)
-            print("\t","ADIOS bytes written    :",adios_write_bytes)
+            print("\t","MPI collective bytes             :",mpi_collective_bytes)
+            print("\t","MPI P2P bytes sent               :",mpi_send_bytes)
+            print("\t","MPI P2P bytes received           :",mpi_recv_bytes)
+            print("\t","Posix IO bytes read              :",io_read_bytes)
+            print("\t","Posix IO bytes written           :",io_write_bytes)
+            print("\t","ADIOS bytes written              :",adios_write_bytes)
+            print("\t","Max Memory Footprint (HWM) (kB)  :",high_water_mark)
+            print("\t","Mean Memory Footprint (RSS) (kB) :",resident_set_size)
             print("")
         
         rc_exe = n_str
@@ -230,7 +240,7 @@ def sos_flow_analysis(run_dir):
         perf_results[rc_exe] = {"time": total_inclusive,
                                "adios_time": adios_exclusive,
                                "adios_data": adios_write_bytes}
-        print perf_results
+        print (perf_results)
         return(perf_results)
 
 if __name__ == '__main__':
