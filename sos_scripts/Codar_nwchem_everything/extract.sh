1ecde1b5f4273ff4b19c4e016ffda692d5b24b0c
Kevin Huck <khuck@cs.uoregon.edu>
2018-10-10 11:29:43 -0400

    working extract of flexpath

--- sos_scripts/Codar_nwchem_everything/extract.sh
+++ sos_scripts/Codar_nwchem_everything/extract.sh
@@ -21,16 +21,20 @@ while [ ! -f ${SOS_WORK}/sosd.00000.key ] ; do
 done
 
 echo "Launching ADIOS trace export from SOS..."
-#python ./tau_trace_adios.py
-python /Codar/nwchem-1/tau_trace_adios.py &
+python /Codar/nwchem-1/tau_trace_adios.py >& /Codar/nwchem-1/sos_extract.out &
 
 # Wait for that to startup
-while [ ! -f ${SOS_WORK}/tau-metrics.db ] ; do
-    echo "Waiting for ${SOS_WORK}/tau-metrics.db..."
+while [ ! -f ${SOS_WORK}/QA/tests/ethanol/tau-metrics.bp ] ; do
+    echo "Waiting for ${SOS_WORK}/QA/tests/ethanol/tau-metrics.bp..."
     sleep 1
 done
 
-bash /Chimbuko/PerformanceAnalysis/drivers/run_chimbuko.sh /Chimbuko/PerformanceAnalysis/drivers/chimbuko_tau-nwchem.cfg
+. /etc/profile
+
+cd /Chimbuko/PerformanceAnalysis/drivers
+export PYTHONPATH=/Chimbuko/PerformanceAnalysis/lib/codar/chimbuko/perf_anom:/Install/adios-1.13.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+python3 chimbuko.py chimbuko_tau-nwchem.cfg >& /Codar/nwchem-1/anomaly.out
 
 # Once the script has finished, exit the daemon
-/Install/sosflow/bin/sosd_stop
+# /Install/sosflow/bin/sosd_stop
+
