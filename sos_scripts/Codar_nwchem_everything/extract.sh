d2f878d1e93d2d68c37ab863a25c6c6066c7f9aa
Kevin Huck <khuck@cs.uoregon.edu>
2018-10-10 13:54:45 -0400

    do sequential processing.

--- sos_scripts/Codar_nwchem_everything/extract.sh
+++ sos_scripts/Codar_nwchem_everything/extract.sh
@@ -21,20 +21,21 @@ while [ ! -f ${SOS_WORK}/sosd.00000.key ] ; do
 done
 
 echo "Launching ADIOS trace export from SOS..."
-python /Codar/nwchem-1/tau_trace_adios.py >& /Codar/nwchem-1/sos_extract.out &
+python /Codar/nwchem-1/tau_trace_adios.py >& /Codar/nwchem-1/sos_extract.out
 
 # Wait for that to startup
 while [ ! -f ${SOS_WORK}/QA/tests/ethanol/tau-metrics.bp ] ; do
     echo "Waiting for ${SOS_WORK}/QA/tests/ethanol/tau-metrics.bp..."
     sleep 1
 done
+sleep 1
 
 . /etc/profile
 
+echo "Launching Anomaly detection..."
 cd /Chimbuko/PerformanceAnalysis/drivers
 export PYTHONPATH=/Chimbuko/PerformanceAnalysis/lib/codar/chimbuko/perf_anom:/Install/adios-1.13.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 python3 chimbuko.py chimbuko_tau-nwchem.cfg >& /Codar/nwchem-1/anomaly.out
 
 # Once the script has finished, exit the daemon
-# /Install/sosflow/bin/sosd_stop
-
+/Install/sosflow/bin/sosd_stop
